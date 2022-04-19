f60dcc7bdc1fd5e1839df2e26d96012a4bc8f537
sencinia <91294669+sencinia@users.noreply.github.com>
2022-04-18 21:43:11 -0700

    Created main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+# 🚨 Don't change the code below 👇
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+bill = 0
+
+if size.upper() == "S":
+  bill += 15
+  if add_pepperoni.upper() == "Y":
+    bill += 2
+elif size.upper() == "M":
+  bill += 20
+  if add_pepperoni.upper() == "Y":
+    bill += 3
+else:
+  bill += 25
+  if add_pepperoni.upper() == "Y":
+    bill += 3
+
+if extra_cheese.upper() == "Y":
+  bill += 1
+
+print(f"Your final bill is: ${bill}")
+
+
+
